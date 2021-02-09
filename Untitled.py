80603c5c75cc6341082f09a6b7c8d41e400e3c5c
Dauren03 <78399681+Dauren03@users.noreply.github.com>
2021-02-09 12:27:30 +0600

    prctica 2

---
+++ Untitled.py
@@ -0,0 +1,294 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+a=4
+b=3
+a**b
+print(a**b)
+
+
+# In[3]:
+
+
+z=1+2j
+print(z)
+
+
+# In[4]:
+
+
+x=complex(3,2)
+print(x)
+
+
+# In[6]:
+
+
+x+z
+
+
+# In[7]:
+
+
+x-z
+
+
+# In[8]:
+
+
+x*z
+
+
+# In[9]:
+
+
+x/z
+
+
+# In[10]:
+
+
+x**z
+
+
+# In[11]:
+
+
+x**3
+
+
+# In[14]:
+
+
+x=3+2j
+x.real
+
+
+# In[15]:
+
+
+x.imag
+
+
+# In[16]:
+
+
+x.conjugate()
+
+
+# In[17]:
+
+
+p=9
+g=3
+p&g
+
+
+# In[18]:
+
+
+p|g
+
+
+# In[19]:
+
+
+p^g
+
+
+# In[20]:
+
+
+~p
+
+
+# In[21]:
+
+
+p<<1
+
+
+# In[22]:
+
+
+p>>1
+
+
+# In[32]:
+
+
+a=2
+
+
+# In[33]:
+
+
+a
+
+
+# In[35]:
+
+
+b=3
+b
+
+
+# In[36]:
+
+
+c=a*b
+print(c)
+
+
+# In[37]:
+
+
+d=a**b
+print(d)
+
+
+# In[38]:
+
+
+d==c
+
+
+# In[39]:
+
+
+d>c
+
+
+# In[40]:
+
+
+z=1+2j
+print(z)
+
+
+# In[41]:
+
+
+y=complex(10.6)
+print(y)
+
+
+# In[42]:
+
+
+v=complex(10.5,8)
+print(v)
+
+
+# In[43]:
+
+
+k=z+v-y
+print(k)
+
+
+# In[44]:
+
+
+m=124504
+hex(m)
+
+
+# In[45]:
+
+
+oct(m)
+
+
+# In[46]:
+
+
+bin(m)
+
+
+# In[47]:
+
+
+import math
+
+
+# In[48]:
+
+
+math.ceil(3.2)
+
+
+# In[49]:
+
+
+math.fabs(-7)
+
+
+# In[50]:
+
+
+math.factorial(5)
+
+
+# In[51]:
+
+
+math.floor(3.2)
+
+
+# In[52]:
+
+
+math.exp(3)
+
+
+# In[53]:
+
+
+math.log2(8)
+
+
+# In[54]:
+
+
+math.log10(1000)
+
+
+# In[55]:
+
+
+math.log(5)
+
+
+# In[56]:
+
+
+math.log(4,8)
+
+
+# In[57]:
+
+
+math.pow(3,4)
+
+
+# In[59]:
+
+
+math.sqrt(25)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
